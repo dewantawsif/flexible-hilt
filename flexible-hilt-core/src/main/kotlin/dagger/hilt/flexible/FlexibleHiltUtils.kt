7a8d7a475d26654573fe9ed5f0520ce7fe95b1ab
Dewan Tawsif <theonetawsif@gmail.com>
2023-11-18 08:21:47 +0600

    Cleanup

--- flexible-hilt-core/src/main/kotlin/dagger/hilt/flexible/FlexibleHiltUtils.kt
+++ flexible-hilt-core/src/main/kotlin/dagger/hilt/flexible/FlexibleHiltUtils.kt
@@ -4,7 +4,7 @@ inline fun <reified T : FlexibleHiltItem> getFromFlexibleHilt(): T {
     return FlexibleHilt.getItems()[T::class.java]?.get() as? T
         ?: error(
             "Couldn't find ${T::class.java} in 'FlexibleHiltGraph'. Make sure you have annotated " +
-                "the base class/interface with [IncludeInFlexibleHiltGraph] and it inherits [FlexibleHiltItem]",
+                "the base class/interface with [MakeFlexible] and it inherits [FlexibleHiltItem]",
         )
 }
 
