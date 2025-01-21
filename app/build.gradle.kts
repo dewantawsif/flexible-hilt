4494b85f2bffe686d2faa6b86155b68e142a73d7
Dewan Tawsif <theonetawsif@gmail.com>
2025-01-22 01:54:04 +0600

    Bump compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,16 +16,15 @@ plugins {
 
 android {
     namespace = "me.tawsif.hilt.sample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.tawsif.hilt.sample"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
